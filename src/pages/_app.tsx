5af91822c8663a12cac1784373df865597fd40bf
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2022-10-07 00:23:03 +0530

    setup header title & basic home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app';
-import './styles/globals.css';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Haxxor Bunny</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
