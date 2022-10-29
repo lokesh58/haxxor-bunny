efa7cc3d796b13140ca8b57518c245727eab8aeb
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2022-10-30 00:39:03 +0530

    update valkyrie done

--- src/commands/manage-valkyries.ts
+++ src/commands/manage-valkyries.ts
@@ -58,7 +58,7 @@ const ManageValkyriesCommand: HaxxorBunnyCommand = {
             type: ApplicationCommandOptionType.String,
             name: 'acronyms',
             description: 'Acronyms of the valkyrie',
-            required: false,
+            required: true,
           },
           {
             type: ApplicationCommandOptionType.String,
@@ -158,8 +158,7 @@ const ManageValkyriesCommand: HaxxorBunnyCommand = {
             acronyms: z
               .string()
               .regex(/^\s*\w+(\s*,\s*\w+)*\s*$/, { message: 'Please use `<acronym> (, ...)` notation' })
-              .transform((v) => v.trim().split(/\s*,\s*/))
-              .optional(),
+              .transform((v) => v.trim().split(/\s*,\s*/)),
             emoji: z.string().regex(SingleEmojiRegex, { message: 'Invalid Emoji' }).optional(),
             'aug-emoji': z.string().regex(SingleEmojiRegex, { message: 'Invalid Emoji' }).optional(),
           })
@@ -169,7 +168,7 @@ const ManageValkyriesCommand: HaxxorBunnyCommand = {
             ...rest,
           })),
       );
-      const { name, acronyms = [] } = args;
+      const { name, acronyms } = args;
       await this.respond({
         type: InteractionResponseType.DeferredChannelMessageWithSource,
       });
@@ -178,11 +177,9 @@ const ManageValkyriesCommand: HaxxorBunnyCommand = {
           embeds: [
             {
               title: 'Create Valkyrie',
-              description: `❌ Valkyrie with name \`${name}\`${
-                acronyms.length
-                  ? ` or acronym as${acronyms.length > 1 ? ' atleast one of' : ''} \`${acronyms.join('`, `')}\``
-                  : ''
-              } already exists`,
+              description: `❌ Valkyrie with name \`${name}\` or acronym as${
+                acronyms.length > 1 ? ' atleast one of' : ''
+              } \`${acronyms.join('`, `')}\` already exists`,
               color: Colors.Red,
             },
           ],
@@ -214,6 +211,17 @@ const ManageValkyriesCommand: HaxxorBunnyCommand = {
               .regex(/^\s*(\+|-)\w+(\s*,\s*(\+|-)\w+)*\s*$/, {
                 message: 'Please use `<+/-><acronym> (, ...)` notation',
               })
+              .transform((v) =>
+                v.split(/\s*,\s*/).reduce(
+                  (acc, da) => {
+                    if (da.startsWith('+')) {
+                      return { ...acc, add: [...acc.add, da.substring(1)] };
+                    }
+                    return { ...acc, remove: [...acc.remove, da.substring(1)] };
+                  },
+                  { add: [], remove: [] } as { add: string[]; remove: string[] },
+                ),
+              )
               .optional(),
             emoji: z.string().regex(SingleEmojiRegex, { message: 'Invalid Emoji' }).optional(),
             'aug-emoji': z.string().regex(SingleEmojiRegex, { message: 'Invalid Emoji' }).optional(),
@@ -225,6 +233,7 @@ const ManageValkyriesCommand: HaxxorBunnyCommand = {
           })),
       );
       const { valk: valkId, ...updateInfo } = args;
+      const { deltaAcronyms, emoji, augEmoji } = updateInfo;
       if (!Object.values(updateInfo).filter(Boolean).length) {
         return this.respond({
           type: InteractionResponseType.ChannelMessageWithSource,
@@ -234,11 +243,44 @@ const ManageValkyriesCommand: HaxxorBunnyCommand = {
           },
         });
       }
-      return this.respond({
-        type: InteractionResponseType.ChannelMessageWithSource,
-        data: {
-          content: JSON.stringify(args),
-        },
+      await this.respond({
+        type: InteractionResponseType.DeferredChannelMessageWithSource,
+      });
+      const valk = await Valkyrie.findById(valkId);
+      if (!valk) {
+        await this.editOriginalResponse({
+          embeds: [
+            {
+              title: 'Update Valkyrie',
+              description: "❌ Given valkyrie doesn't exist",
+              color: Colors.Red,
+            },
+          ],
+        });
+        return;
+      }
+      if (deltaAcronyms) {
+        const acronymSet = new Set(valk.acronyms.map((a) => a.toLowerCase()));
+        deltaAcronyms.add.forEach((a) => {
+          if (!acronymSet.has(a.toLowerCase())) {
+            valk.acronyms.push(a);
+            acronymSet.add(a.toLowerCase());
+          }
+        });
+        const removeSet = new Set(deltaAcronyms.remove.map((a) => a.toLowerCase()));
+        valk.acronyms = valk.acronyms.filter((va) => !removeSet.has(va.toLowerCase()));
+      }
+      if (emoji) valk.emoji = emoji;
+      if (augEmoji) valk.augEmoji = augEmoji;
+      await valk.save();
+      await this.editOriginalResponse({
+        embeds: [
+          {
+            title: 'Update Valkyrie',
+            description: `✅ Valkyrie ${valk.name} updated successfully`,
+            color: Colors.Green,
+          },
+        ],
       });
     }
 
