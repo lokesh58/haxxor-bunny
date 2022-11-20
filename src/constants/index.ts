0f4609abf9d980d77256a93f62917bfdf16b4e72
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2022-11-20 15:44:01 +0530

    set base url dynamically

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,14 @@
 export const BotName = 'Haxxor Bunny';
 
-export const BotHomepageUrl = 'https://haxxor-bunny.up.railway.app';
+export const BotBaseUrl = process.env.RAILWAY_STATIC_URL
+  ? `https://${process.env.RAILWAY_STATIC_URL}`
+  : `http://localhost:${process.env.PORT ?? 3000}`;
+
+export const BotHomepageUrl = BotBaseUrl;
 
 export const InternalBotInviteUrl = '/invite';
 
-export const BotInviteUrl = `${BotHomepageUrl}${InternalBotInviteUrl}`;
+export const BotInviteUrl = `${BotBaseUrl}${InternalBotInviteUrl}`;
 
 export const ExternalBotInviteUrl =
   'https://discord.com/api/oauth2/authorize?client_id=823101978309427221&scope=applications.commands';
