56c6b3222d8907abde3f2f3339506d6abd1e6dcb
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2022-11-20 16:27:49 +0530

    add share url command

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,5 +4,7 @@ export const BotHomepageUrl = '/';
 
 export const BotInviteUrl = '/invite';
 
+export const UserValksUrl = '/user-valks';
+
 export const ExternalBotInviteUrl =
   'https://discord.com/api/oauth2/authorize?client_id=823101978309427221&scope=applications.commands';
