798ecbe018dc03648a3f918160887fc93ddeffbd
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2022-11-27 13:53:04 +0530

    script to upload emoji

---
+++ src/scripts/upload-discord-emoji.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import path from 'path';
+import { uploadDiscordEmojiToCDN } from '../utils/cdn';
+
+async function main() {
+  dotenv.config({ path: path.join(__dirname, '../../.env.local') });
+  const [, , emoji] = process.argv;
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log('Connected to MongoDB');
+    console.log(`Uploading Emoji ${emoji} to DB`);
+    await uploadDiscordEmojiToCDN(emoji);
+    console.log('Upload complete!');
+  } finally {
+    await mongoose.connection.close();
+  }
+}
+
+main();
