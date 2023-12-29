2b9df444240e872f882b995733519a4ea6670ee2
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2023-12-30 00:19:12 +0530

    update compose file to allow variable host port

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,5 @@ export const getBaseAppUrl = (): string => {
   if (process.env.STATIC_URL) {
     return `https://${process.env.STATIC_URL}`;
   }
-  return `http://localhost:${process.env.PORT ?? 3000}`;
+  return `http://localhost:${process.env.HOST_PORT ?? 3000}`;
 };
